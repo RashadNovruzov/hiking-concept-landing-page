bbae27b870309ff62bf6efa6327187d7352a9bb6
RashadNovruzov <r4shad.novruz0v@gmail.com>
2022-07-05 00:05:58 +0400

    Header is ready

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+
+    $('.burger-menu').on('click',function(){
+        $('.header-menu').slideToggle();
+        $('.header-search').slideToggle();
+      })
+
+});
